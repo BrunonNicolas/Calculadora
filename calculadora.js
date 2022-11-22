4b832ba3b42507d0b63f43d2a989cea1c122e572
Bruno Birman <slmds1773@gmail.com>
2022-11-21 21:48:59 -0300

    Tarea 5: Calculadora con JS y Bootstrap

--- calculadora.js
+++ calculadora.js
@@ -1,35 +1,25 @@
 
-let numero1 = parseInt(prompt("ingrese un numero"));
-let operacion  = prompt("Ingrese una operacion + - / * ");
-let numero2 = parseInt(prompt("ingrese un numero"));
-let resultado;
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
 
-
-switch(operacion) {
-    case "+":
-        resultado=(numero1)+(numero2)
-        alert("Tu resultado es: " + resultado);
-   break;
-   case "-":
-    resultado=(numero1)-(numero2)
-    alert("Tu resultado es: " + resultado);
-break;
-case "*":
-        resultado=(numero1)*(numero2)
-        alert("Tu resultado es: " + resultado);
-   break;
-   case "/":
-        resultado=(numero1)/(numero2)
-        alert("Tu resultado es: " + resultado);
-        if(numero2>0){
-            alert("Tu resultado es: " + resultado);
-        }else{
-            alert("ERROR:NO SE PUEDE DIVIDIR POR 0")
-        
-        }
-   break;
-   default:
-   alert("ERROR:Ingrese una operación válida (+ - * /)")
-  break;
-
-}
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    
+    } else if (item.id == "backspace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+     
+    } else if (display.innerText != "" && item.id == "equal") {
+      display.innerText = eval(display.innerText);
+    
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Syntax Error";
+      setTimeout(()=>display.innerText="", 2000);
+    
+    }else{
+        display.innerText += item.id
+    }
+  };
+});
